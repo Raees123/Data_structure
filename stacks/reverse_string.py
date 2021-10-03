8d1defecec4020bb66098c565221737667b5d84b
raees123 <raiskochargi@gmail.com>
2021-10-03 20:22:11 +0530

    Implement reverse a string and balanced brackets using stack in python

---
+++ stacks/reverse_string.py
@@ -0,0 +1,41 @@
+def CreateStack():
+    stack = []
+    return stack
+
+
+def size(stack):
+    return len(stack)
+
+
+def isEmpty(stack):
+    if size(stack) == 0:
+        return True
+
+
+def push(stack,item):
+    stack.append(item)
+
+
+def pop(stack):
+    if isEmpty(stack): return
+    return stack.pop()
+
+
+def reverse(string):
+    n = len(string)
+
+    stack = CreateStack()
+
+    for i in range(0,n,1):
+        push(stack,string[i])
+
+    string=""
+
+    for i in range(0,n,1):
+        string+=pop(stack)
+
+    return string
+
+string="Restart"
+string = reverse(string)
+print("Reversed string is " + string)
